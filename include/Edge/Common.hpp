57d16b97bb23bbb15c367addd6ba8fe78719b657
Infiltratist <marluxiiiii@gmail.com>
2015-03-04 18:49:58 -0500

    Absolutely Nothing
    Literally nothing

---
+++ include/Edge/Common.hpp
@@ -0,0 +1,12 @@
+#ifndef EDGE_COMMON_HPP
+#define EDGE_COMMON_HPP
+
+namespace Edge
+{
+	using Float32 = float;
+	using Float64 = double;
+
+} //namespace Edge
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Edge/Common.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello!\n";
+	std::cin.ignore();
+
+	return 0;
+}
